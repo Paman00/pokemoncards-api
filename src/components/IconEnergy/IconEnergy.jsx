d574d6ad1047d5d4e09700583d033827eacc234f
Paman00 <angelurano00@gmail.com>
2023-03-06 07:29:14 +0100

    Fix images typos and directories

--- src/components/IconEnergy/IconEnergy.jsx
+++ src/components/IconEnergy/IconEnergy.jsx
@@ -1,7 +1,34 @@
 import { IconImg } from "./StyledIcon";
+import colorless from "../../assets/images/energy/colorless.png";
+import darkness from "../../assets/images/energy/darkness.png";
+import dragon from "../../assets/images/energy/dragon.png";
+import fairy from "../../assets/images/energy/fairy.png";
+import fighting from "../../assets/images/energy/fighting.png";
+import fire from "../../assets/images/energy/fire.png";
+import grass from "../../assets/images/energy/grass.png";
+import lightning from "../../assets/images/energy/lightning.png";
+import metal from "../../assets/images/energy/metal.png";
+import psychic from "../../assets/images/energy/psychic.png";
+import water from "../../assets/images/energy/water.png";
+
+const energyTypes = {
+	colorless,
+	darkness,
+	dragon,
+	fairy,
+	fighting,
+	fire,
+	grass,
+	lightning,
+	metal,
+	psychic,
+	water,
+};
 
 const IconEnergy = ({ type }) => {
-    return (<IconImg src={`../src/assets/images/${type}energy.png`} alt={`${type} energy`} />)
-}
+	return (
+		<IconImg src={energyTypes[type.toLowerCase()]} alt={`${type} energy`} />
+	);
+};
 
 export { IconEnergy };
